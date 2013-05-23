be7a0fb45ccb5077317b56cd194f7a764c58d8b1
cerebrl <justin@cerebrl.com>
2013-05-23 18:38:56 -0500

    Stupid subtree crap.

--- phi/app/app.js
+++
@@ -1,2 +0,0 @@
-// This is the module that represents the entire application
-var PHI = angular.module('PHI', []);
--- phi/app/filters/fltr-example.js
+++
@@ -1,5 +0,0 @@
-PHI.filter('interpolate', ['version', function(version) {
-		return function(text) {
-			return String(text).replace(/\%VERSION\%/mg, version);
-		}
-	}]);
